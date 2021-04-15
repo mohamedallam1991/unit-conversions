764925a0b212b3bff7031cdeb80d315550a53180
mohamedallam1991 <mohamedallam1991@users.noreply.github.com>
2021-04-15 12:30:20 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,33 +4,23 @@ namespace Mohamedallam1991\UnitConversions;
 
 class Weight
 {
-	private float $kilograms;
+    private float $kilograms;
 
-	public static function fromKilograms(float $kilograms): self
-	{
-		return new static($kilograms);
-	}
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
 
-	public function __construct(float $kilograms)
-	{
-		$this->kilograms = $kilograms;
-	}
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
 
-
-	//kg to lb
-	// kg X 220.4623
-	// 2.204623
-	public function toLbs()
-	{
-		return $this->kilograms * 2.204623;
-	}
-
-
-
-
-
-
-
-
-	
+    //kg to lb
+    // kg X 220.4623
+    // 2.204623
+    public function toLbs()
+    {
+        return $this->kilograms * 2.204623;
+    }
 }
